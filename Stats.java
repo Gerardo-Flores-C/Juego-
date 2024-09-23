628e7827672c2084343d9cac2f7c0cfede26dd3e
Gerardo-Flores-C <L23280045@toluca.tecnm.mx>
2024-09-22 23:18:56 -0600

    Create Stats.java

---
+++ Stats.java
@@ -0,0 +1,15 @@
+public enum Stats {
+    HP,
+    MAX_HP,
+    MP,
+    MAX_MP,
+    ATTACK,
+    DEFENSE,
+    SPEED,
+    DEXTERITY,
+    LUCK,
+    ACCURACY,
+    EVASION,
+    CRITICAL_HIT_CHANCE,
+    CRITICAL_HIT_DAMAGE
+}
